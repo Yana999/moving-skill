ae3f5f1a9bcee31e9497d906db595471a0e242ba
Yana999 <abdramanova.yana@yandex.ru>
2023-05-28 02:34:07 +0300

    добавила messageBus

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
 import logging
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft_bus_client import MessageBusClient, Message
 
 
 class Moving(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        print('Setting up client to connect to a local mycroft instance')
+        self.client = MessageBusClient()
+        self.client.run_in_thread()
 
     @intent_file_handler('moving.intent')
     def handle_moving(self, message):
@@ -12,17 +16,29 @@ class Moving(MycroftSkill):
         utt = str(utt)
         utt = utt[6:]
         logging.info("Полученный текст: " + utt)
+        print('Sending speak message...')
+        direct = "Не могу распознать направление"
         if (utt in ["вправо", "право", "направо"]):
-            logging.info("Едем направо")
+            direct = "Едем направо"
+            logging.info(direct)
+            self.client.emit(Message('speak', data={'direction': 3}))
         if (utt in ["влево", "лево", "налево"]):
-            logging.info("Едем налево")
+            direct = "Едем налево"
+            logging.info(direct)
+            self.client.emit(Message('speak', data={'direction': 2}))
         if (utt in ["вперед", "прямо"]):
-            logging.info("Едем прямо")
+            direct = "Едем прямо"
+            logging.info()
+            self.client.emit(Message('speak', data={'direction': 1}))
         if (utt in ["назад"]):
-            logging.info("Едем назад")
+            direct = "Едем назад"
+            logging.info(direct)
+            self.client.emit(Message('speak', data={'direction': 4}))
         if (utt in ["стой", "стоять", "остановись", "стоп"]):
-            logging.info("Стоим")
-        self.speak_dialog('moving')
+            direct = "Стоим"
+            logging.info(direct)
+            self.client.emit(Message('speak', data={'utterance': 'Hello World'}))
+        self.speak(direct)
 
 
 def create_skill():
